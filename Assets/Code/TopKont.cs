8ef1a5546dc84c2655494fd25fb1f56fc4403a72
furkidem <furkan.demirel.056@gmail.com>
2021-02-19 21:19:53 +0300

    coder

---
+++ Assets/Code/TopKont.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopKont : MonoBehaviour
+{
+    Rigidbody rigid;
+    public int hız = 5;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+        Vector3 vec3 = new Vector3(yatay, 0, dikey);
+        rigid.AddForce(vec3 * hız);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        string isim = other.gameObject.tag;
+        if (isim=="engel")
+        {
+            Destroy(other.gameObject);
+        }
+       
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        string isim = collision.gameObject.tag;
+        if (isim == "duvar")
+        {
+            Debug.Log("gg");
+        }
+    }
+}
---
+++ Assets/Code/topObje.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topObje : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
